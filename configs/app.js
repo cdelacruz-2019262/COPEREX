eed452c50cb99df73587385ada8a96373f01e0b2
cdelacruz-2019262 <cdelacruz-2019262@kinal.edu.gt>
2024-02-29 23:45:00 -0600

    usuario terminado

---
+++ configs/app.js
@@ -0,0 +1,29 @@
+'use strict'
+
+import express from "express"
+import { config } from "dotenv"
+import morgan from "morgan"
+import helmet from 'helmet'
+import cors from 'cors'
+import userRoutes from '../src/user/user.routes.js'
+
+//configs
+const app = express()
+config()
+const port = process.env.PORT || 3056
+
+//configuraciones del servidor
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(cors()) 
+app.use(helmet()) 
+app.use(morgan('dev')) 
+
+//declaracion de rutas
+app.use('/user', userRoutes)
+
+//levantar el server(revisar que si prende)
+export const initServer = ()=>{
+    app.listen(port)
+    console.log(`server is running in port ${port}`)
+}
---
+++ src/user/user.model.js
@@ -0,0 +1,35 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    surname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: true
+    },
+    password: {
+        type: String,
+        minLength: [8, 'Password must be 8 characters'],
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true
+    }
+})
+
+//pre mongoose
+export default model('user', userSchema)
